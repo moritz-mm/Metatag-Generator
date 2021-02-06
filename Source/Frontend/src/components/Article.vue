aa5cfde6bf2f8aa45c82dd790d0a573ca703207e
Moritz Müller <moritz-m@outlook.de>
2021-02-06 13:00:06 +0100

    Use the code tag for code examples

--- Source/Frontend/src/components/Article.vue
+++ Source/Frontend/src/components/Article.vue
@@ -12,15 +12,15 @@
             Was sind die wichtigsten Metatags?
         </h1>
         <blockquote class="text h-6">
-            Hier sind die wichtigsten Metatags nach Priorität für Suchmaschinenoptimierung (SEO) sortiert beschrieben:
+            Hier sind die <strong>wichtigsten Metatags</strong> nach Priorität für <strong>Suchmaschinenoptimierung</strong> (SEO) sortiert beschrieben:
         </blockquote>
 
         <h2 class="text h-3">
             1. Der Titel Tag
         </h2>
-        <p class="text h-5">
-            HTML: &lt;title&gt;...&lt;/title&gt;
-        </p>
+        <code class="box neutral round p-a-1 text h-6">
+            &lt;title&gt;Hier kommt dein Titel rein.&lt;/title&gt;
+        </code>
         <blockquote class="text h-6">
             Der Titel Tag ist das Erste, was ein User in den Suchergebnissen von ihrer Webseite sieht, deswegen ist er der 
             wichtigste Tag für Suchmaschinenoptimierung (SEO). 
@@ -31,7 +31,7 @@
         <ul class="text h-6">
             <li>• Maximal <strong>70 Zeichen</strong> lang.</li>
             <li>• Primärkeyword muss am Anfang stehen.</li>
-            <li>• Füg Zahlen hinzu z.B. Die <strong>5</strong> wichtigsten Keywörter für SEO in 2021.</li>
+            <li>• Füg Zahlen hinzu z.B. "Die <strong>5</strong> wichtigsten Keywörter für SEO in 2021."</li>
             <li>• Benutze nicht zu viele Keywörter. (Keyword stuffing)</li>
             <li>• Jede Seite sollte ein einzigartigen Titel haben.</li>
         </ul>
@@ -40,9 +40,9 @@
         <h2 class="text h-3">
             2. Die Meta Beschreibung
         </h2>
-        <p class="text h-5">
-            HTML: &lt;meta name="description" content="..." /&gt;
-        </p>
+        <code class="box neutral round p-a-1 text h-6">
+            &lt;meta name="description" content="Hier kommt deine Meta Beschreibung rein." /&gt;
+        </code>
         <blockquote class="text h-6">
             Die Beschreibung wird von Suchmaschinen wie Google zwar nicht mehr für Suchmaschinenoptimierung (SEO) verwendet, 
             ist aber dennoch wichtig für die Webpräsenz. Der Inhalt von <strong>content="..."</strong> wird in den Suchergebnissen direkt unter dem Titel angezeigt.
@@ -61,9 +61,9 @@
         <h2 class="text h-3">
             3. Der alternativ Text für Bilder
         </h2>
-        <p class="text h-5">
-            HTML: &lt;img alt="..." /&gt;
-        </p>
+        <code class="box neutral round p-a-1 text h-6">
+            &lt;img src="..." alt="Hier kommt die Bildbeschreibung rein." /&gt;
+        </code>
         <blockquote class="text h-6">
             Das Alt-Tag ist für alle Bilder wichtig, da Suchmaschinen nicht alle Bilder lesen können.
             Daher muss jeder <strong>&lt;img /&gt;</strong> Tag ein Alt-Text haben, damit die Suchmaschine das Bild lesen kann.
@@ -86,23 +86,23 @@
         <h3 class="text h-5">
             Open Graph Tags:
         </h3>
-        <ul class="text h-6">
-            <li>• &lt;meta property="og:type" content="website" /&gt;</li>
-            <li>• &lt;meta property="og:title" content="Der Titel von deiner Webseite" /&gt;</li>
-            <li>• &lt;meta property="og:description" content="Die Beschreibung von deiner Webseite" /&gt;</li>
-            <li>• &lt;meta property="og:image" content="Ein Vorschau Bild als Url" /&gt;</li>
-            <li>• &lt;meta property="og:url" content="Die Url deiner Webseite" /&gt;</li>
-        </ul>
+        <code class="box neutral round p-a-1 text h-6">
+            &lt;meta property="og:type" content="website" /&gt; <br />
+            &lt;meta property="og:title" content="Der Titel von deiner Webseite" /&gt; <br />
+            &lt;meta property="og:description" content="Die Beschreibung von deiner Webseite" /&gt; <br />
+            &lt;meta property="og:image" content="Ein Vorschau Bild als Url" /&gt; <br />
+            &lt;meta property="og:url" content="Die Url deiner Webseite" /&gt; <br />
+        </code>
         <h3 class="text h-5">
             Twitter Cards:
         </h3>
-        <ul class="text h-6">
-            <li>• &lt;meta property="twitter:card" content="summary_large_image" /&gt;</li>
-            <li>• &lt;meta property="twitter:title" content="Der Titel von deiner Webseite" /&gt;</li>
-            <li>• &lt;meta property="twitter:description" content="Die Beschreibung von deiner Webseite" /&gt;</li>
-            <li>• &lt;meta property="twitter:image" content="Ein Vorschau Bild als Url" /&gt;</li>
-            <li>• &lt;meta property="twitter:url" content="Die Url deiner Webseite" /&gt;</li>
-        </ul>
+        <code class="box neutral round p-a-1 text h-6">
+            &lt;meta property="twitter:card" content="summary_large_image" /&gt; <br />
+            &lt;meta property="twitter:title" content="Der Titel von deiner Webseite" /&gt; <br />
+            &lt;meta property="twitter:description" content="Die Beschreibung von deiner Webseite" /&gt; <br />
+            &lt;meta property="twitter:image" content="Ein Vorschau Bild als Url" /&gt; <br />
+            &lt;meta property="twitter:url" content="Die Url deiner Webseite" /&gt; <br />
+        </code>
     </article>
 </template>
 
