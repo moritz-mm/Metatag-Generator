f124acc4675533a73d40de22af6d8030ed30e593
Moritz Müller <moritz-m@outlook.de>
2021-02-08 12:20:23 +0100

    Add some high quality links

--- Source/Frontend/src/components/Article.vue
+++ Source/Frontend/src/components/Article.vue
@@ -40,7 +40,7 @@
             <li>• Maximal <strong>70 Zeichen</strong> lang.</li>
             <li>• Primärkeyword muss am Anfang stehen.</li>
             <li>• Füg Zahlen hinzu z.B. "Die <strong>5</strong> wichtigsten Keywörter für SEO in 2021."</li>
-            <li>• Benutze nicht zu viele Keywörter. (Keyword stuffing)</li>
+            <li>• Benutze nicht zu viele Keywörter. (<a class="link primary" target="_blank" rel="noreferrer noopener" href="https://www.seo-kueche.de/lexikon/keyword-stuffing/">Keyword stuffing</a>)</li>
             <li>• Jede Seite sollte ein einzigartigen Titel haben.</li>
         </ul>
 
@@ -52,7 +52,7 @@
             &lt;meta name="description" content="Hier kommt deine Meta Beschreibung rein." /&gt;
         </code>
         <blockquote class="text h-6">
-            Die Beschreibung wird von Suchmaschinen wie Google zwar nicht mehr für Suchmaschinenoptimierung (SEO) verwendet, 
+            Die Beschreibung wird von Suchmaschinen wie Google zwar nicht mehr für <a class="link primary" target="_blank" rel="noreferrer noopener" href="https://de.wikipedia.org/wiki/Suchmaschinenoptimierung">Suchmaschinenoptimierung (SEO)</a> verwendet, 
             ist aber dennoch wichtig für die Webpräsenz. Der Inhalt von <strong>content="..."</strong> wird in den Suchergebnissen direkt unter dem Titel angezeigt.
         </blockquote>
         <p class="text h-6">
@@ -62,7 +62,7 @@
             <li>• Maximal <strong>160 Zeichen</strong> lang, sonst wird der Text abgeschnitten.</li>
             <li>• Keywörter von dem Titel müssen enthalten sein.</li>
             <li>• Genau Beschreiben, was man von der Webseite zu erwarten hat.</li>
-            <li>• <strong>Keine</strong> Aufforderung wie: "Kaufen sie jetzt!".</li>
+            <li>• <strong>Keine</strong> Aufforderung wie: "Kaufen sie jetzt!". (<a class="link primary" target="_blank" rel="noreferrer noopener" href="https://de.wikipedia.org/wiki/Call_to_Action_(Marketing)">Call-to-action</a>)</li>
         </ul>
 
 
