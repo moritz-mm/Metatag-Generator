b727921f50887bf3f5aa1c88b14f3566d30f9e0b
Moritz Müller <moritz-m@outlook.de>
2021-02-10 21:32:23 +0100

    Null checking for parse method

--- Source/Frontend/src/modules/Generator.ts
+++ Source/Frontend/src/modules/Generator.ts
@@ -19,20 +19,22 @@ export async function fetchMetaTags(url: string) {
         headers: {
             "Content-Type": "text/html"
         }
-    }).catch();
+    }).catch(err => console.warn(`Die Website zu der Url "${url}" wurde nicht gefunden.`));
 
-    const domParser = new DOMParser();
-    const dom = domParser.parseFromString(await response.text(), "text/html");
-
-    specifications.website = response.url;
-    specifications.title = dom.getElementsByTagName("title")[0].text;
-    const metaTags = document.getElementsByTagName("meta");
-    for (let i = 0; i < metaTags.length; i++) {
-        const meta = metaTags[i];
-        if (meta.name === "description") {
-            specifications.description = meta.content;
-        } else if (meta.getAttribute("property") === "og:image" || "twitter:image") {
-            specifications.imageUrl = meta.content;
+    if (response !== null) {
+        const domParser = new DOMParser();
+        const dom = domParser.parseFromString(await response.text(), "text/html");
+    
+        specifications.website = response.url;
+        specifications.title = dom.getElementsByTagName("title")[0].text;
+        const metaTags = document.getElementsByTagName("meta");
+        for (let i = 0; i < metaTags.length; i++) {
+            const meta = metaTags[i];
+            if (meta.name === "description") {
+                specifications.description = meta.content;
+            } else if (meta.getAttribute("property") === "og:image" || "twitter:image") {
+                specifications.imageUrl = meta.content;
+            }
         }
     }
 }
