a1dfecdce9b8cc59f303ea02b14fc4dd09ade7a3
Moritz Müller <moritz-m@outlook.de>
2021-02-14 16:26:29 +0100

    Add SSG

--- Source/Frontend/src/modules/TextMesurer.ts
+++ Source/Frontend/src/modules/TextMesurer.ts
@@ -2,13 +2,16 @@ export const googleTitleFont = "normal 20px arial, sans-serif";
 export const googleDescriptionFont = "normal 13px arial, sans-serif";
 
 export function measureTextWidth(text: string, font: string): number {
-    const canvas = (document.getElementById("canvasId") || document.createElement("canvas")) as HTMLCanvasElement;
-    canvas.setAttribute("id", "canvasId");
-    const context = canvas.getContext("2d");
-    let metrics = -1;
-    if (context !== null) {
-        context.font = font;
-        metrics = context.measureText(text).width;
+    if (typeof document !== 'undefined') {
+        const canvas = (document.getElementById("canvasId") || document.createElement("canvas")) as HTMLCanvasElement;
+        canvas.setAttribute("id", "canvasId");
+        const context = canvas.getContext("2d");
+        let metrics = -1;
+        if (context !== null) {
+            context.font = font;
+            metrics = context.measureText(text).width;
+        }
+        return Math.round(metrics);
     }
-    return Math.round(metrics);
+    return -1;
 }
--- Source/Frontend/vite.config.ts
+++ Source/Frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [Vue()],
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+  }
+});
